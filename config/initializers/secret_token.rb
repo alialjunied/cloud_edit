3ec9600f840668a5b21c73337176a523bf60b1f1
Ali Aljunied <alialjunied@gmail.com>
2013-09-11 23:15:01 +0800

    part 1 of cloud edit tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CloudEdit::Application.config.secret_key_base = 'dd0b1bbcecbcfe1e303c614d4efcd34c40bf6c13e5588a115330320b0fcaf06a3139bfa5911c715edcb85bfb5a93bcc2ad295ac8f73d6a029c4cf440dd71243f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CloudEdit::Application.config.session_store :cookie_store, key: '_cloud_edit_session'
